4fb864561e6c8dd3eae5b1c4013e3db04bb93d19
Jad Jabbour <jabbourj.87@gmail.com>
2013-06-06 15:02:01 +0300

    first upload

---
+++ TabWebControl/TabControl.cs
@@ -0,0 +1,204 @@
+﻿namespace TabWebControl
+{
+    using System;
+    using System.Web.UI;
+    using System.Drawing;
+    using System.ComponentModel;
+    using System.Web.UI.WebControls;
+
+    [
+        ParseChildren(true, "TabPages"),
+        ToolboxData("<{0}:MyTabControl runat=\"server\" ></{0}:MyTabControl>"),
+        PersistChildren(false),
+        Designer(typeof(TabControlDesigner))
+    ]
+    public class TabControl : CompositeControl
+    {
+        #region private fields
+        private TabPageCollection _tabPages;
+        private int _currentDesignTab;
+        private int _selectedTab;
+        #endregion private fields
+
+        #region public properties
+        [
+            PersistenceMode(PersistenceMode.InnerProperty),
+            DesignerSerializationVisibility(DesignerSerializationVisibility.Visible),
+            MergableProperty(false)
+        ]
+        public TabPageCollection TabPages
+        {
+            get
+            {
+                if (_tabPages == null)
+                {
+                    _tabPages = new TabPageCollection();
+                }
+                return _tabPages;
+            }
+        }
+
+        /// <summary>
+        /// Get or set the deesign time active tab.
+        /// </summary>
+        [
+            Browsable(false),
+            PersistenceMode(PersistenceMode.InnerProperty),
+            DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)
+        ]
+        public int CurrentDesignTab
+        {
+            get { return _currentDesignTab; }
+            set { _currentDesignTab = value; }
+        }
+
+        /// <summary>
+        /// Get or set the runtime active tab.
+        /// </summary>
+        public int SelectedTab
+        {
+            get { return _selectedTab; }
+            set { _selectedTab = value; }
+        }
+        #endregion public properties
+
+        #region private methods
+        private void BuildTitles(Table tabControlTable)
+        {
+            // Create the titles row
+            TableRow titlesRow = new TableRow();
+            titlesRow.HorizontalAlign = HorizontalAlign.Center;
+
+            int i = 0;
+            foreach (TabPage tabPage in _tabPages)
+            {
+                // Create titles cells
+                TableCell tabTitleCell = new TableCell();
+                tabTitleCell.Text = tabPage.Title;
+                tabTitleCell.Width = new Unit("");
+                tabTitleCell.BorderStyle = BorderStyle.Outset;
+                tabTitleCell.BorderWidth = new Unit("2");
+                tabTitleCell.Style["padding"] = "0px 4px 0px 4px";
+                tabTitleCell.Style["cursor"] = "hand";
+                tabTitleCell.Wrap = false;
+                tabTitleCell.Height = new Unit("20");
+                if (!DesignMode)
+                {
+                    //Highlight the selected tab title
+                    if (_selectedTab == i)
+                    {
+                        tabTitleCell.Style["background-color"] = ColorTranslator.ToHtml(Color.DarkGray);
+                    }
+                }
+
+                //Add on-click event on the title cell to switch between tabs
+                tabTitleCell.Attributes.Add("onclick", "ShowTab(this, " + i.ToString() + ")");
+
+                titlesRow.Cells.Add(tabTitleCell);
+                i++;
+            }
+
+            //Add additional empty cell
+            TableCell tc1 = new TableCell();
+            tc1.Width = new Unit("100%");
+            tc1.Height = new Unit("20");
+            titlesRow.Cells.Add(tc1);
+            titlesRow.Height = new Unit("20");
+            tabControlTable.Rows.Add(titlesRow);
+        }
+
+        private void BuildContentRows(Table tabControlTable)
+        {
+            // Create content row(s)
+
+            if (DesignMode)
+            {
+                TableRow contentRow = new TableRow();
+                TableCell contentCell = BuildContentCell(contentRow);
+                _tabPages[_currentDesignTab].TabBody.InstantiateIn(contentCell);
+                tabControlTable.Rows.Add(contentRow);
+            }
+            else
+            {
+                int counter = 0;
+                foreach (TabPage tabPage in _tabPages)
+                {
+                    TableRow contentRow = new TableRow();
+                    TableCell contentCell = BuildContentCell(contentRow);
+                    if (tabPage.TabBody != null)
+                    {
+                        tabPage.TabBody.InstantiateIn(contentCell);
+                    }
+
+                    //only the selected tab body should be visible
+                    if (_selectedTab == counter)
+                    {
+                        contentRow.Style["display"] = "block";
+                    }
+                    else
+                    {
+                        contentRow.Style["display"] = "none";
+                    }
+                    contentRow.Cells.Add(contentCell);
+                    tabControlTable.Rows.Add(contentRow);
+
+                    counter++;
+                }
+            }
+        }
+
+        private TableCell BuildContentCell(TableRow tableRow)
+        {
+            TableCell tc = new TableCell();
+            tc.ColumnSpan = _tabPages.Count + 1;
+            tc.BackColor = Color.White;
+            tc.BorderWidth = new Unit("1");
+            tc.BorderStyle = BorderStyle.Ridge;
+            tc.BorderColor = Color.Silver;
+            tc.Style["padding"] = "5px 5px 5px 5px";
+            tc.Height = new Unit("100%");
+
+            tableRow.Cells.Add(tc);
+
+            return tc;
+        }
+        #endregion private methods
+
+        #region implementations
+        protected override void OnPreRender(EventArgs e)
+        {
+            base.OnPreRender(e);
+            if (DesignMode)
+            {
+                _tabPages[_currentDesignTab].TabBody.InstantiateIn(this);
+            }
+        }
+
+        protected override void CreateChildControls()
+        {
+            // Always start with a clean form
+            Controls.Clear();
+
+            // Create a table using the control's declarative properties
+            Table tabControlTable = new Table();
+            tabControlTable.CellSpacing = 1;
+            tabControlTable.CellPadding = 0;
+            tabControlTable.BorderStyle = BorderStyle;
+            tabControlTable.Width = this.Width;
+            tabControlTable.Height = this.Height;
+            tabControlTable.BackColor = ColorTranslator.FromHtml("inactiveborder");
+
+            //keep a the selected tab index in a an attribute
+            tabControlTable.Attributes.Add("ActiveTabIdx", _selectedTab.ToString());
+
+            BuildTitles(tabControlTable);
+
+            BuildContentRows(tabControlTable);
+
+            // Add the finished tabControlTable to the Controls collection
+            Controls.Add(tabControlTable);
+        }
+
+        #endregion implementations
+    }
+}
---
+++ TabWebControl/TabControlDesigner.cs
@@ -0,0 +1,167 @@
+﻿namespace TabWebControl
+{
+    using System;
+    using System.Web.UI;
+    using System.ComponentModel;
+    using System.Web.UI.Design;
+    using System.Web.UI.WebControls;
+    using System.Security.Permissions;
+    using System.ComponentModel.Design;
+    using System.Web.UI.Design.WebControls;
+
+    [SecurityPermission(SecurityAction.Demand, UnmanagedCode = true)]
+    class TabControlDesigner : CompositeControlDesigner
+    {
+        #region privates
+        private const string HEADER_PREFIX = "Header";
+        private const string CONTENT_PREFIX = "Content";
+        TabControl tabControl;
+        #endregion privates
+
+        #region implementations
+        public override void Initialize(IComponent component)
+        {
+            // Initialize the base
+            base.Initialize(component);
+
+            tabControl = (TabControl)component;
+        }
+
+        public override bool AllowResize
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override String GetDesignTimeHtml(DesignerRegionCollection regions)
+        {
+            int i = 0;
+
+            //add design regions for all header cells, the region name will be prefixd with 
+            // HEADER_PREFIX, and extended with the tab page index. 
+
+            foreach (TabPage tabPage in tabControl.TabPages)
+            {
+                regions.Add(new DesignerRegion(this, HEADER_PREFIX + i.ToString()));
+                i++;
+            }
+
+            // Create an editable region and add it to the regions
+            // the design region name will take CONTENT_PREFIX as prefix and the index
+            // of the current active tab.
+
+            EditableDesignerRegion editableRegion =
+                new EditableDesignerRegion(this,
+                    CONTENT_PREFIX + tabControl.CurrentDesignTab, false);
+            regions.Add(editableRegion);
+
+            // Set the highlight for the selected region
+            regions[tabControl.CurrentDesignTab].Highlight = true;
+
+            // Use the base class to render the markup
+            return base.GetDesignTimeHtml();
+        }
+
+        protected override void CreateChildControls()
+        {
+            base.CreateChildControls();
+
+            // Get a reference to the table, which is the first child control
+            Table table = (Table)tabControl.Controls[0];
+
+            // Add design time markers for all cells which represents the tab body
+            if (table != null)
+            {
+                for (int i = 0; i < tabControl.TabPages.Count; i++)
+                {
+                    table.Rows[0].Cells[i].Attributes[DesignerRegion.DesignerRegionAttributeName] = i.ToString();
+                }
+
+                //set the editable region
+                table.Rows[1].Cells[0].Attributes[DesignerRegion.DesignerRegionAttributeName] = (tabControl.TabPages.Count).ToString();
+            }
+        }
+
+        protected override void OnClick(DesignerRegionMouseEventArgs e)
+        {
+            if (e.Region == null)
+                return;
+
+            // If the clicked region is not a header, return
+            if (e.Region.Name.IndexOf(HEADER_PREFIX) != 0)
+                return;
+
+            // Switch the current view if required
+            //only when the clicked region is different than the active region.
+            if (e.Region.Name.Substring(HEADER_PREFIX.Length) != tabControl.CurrentDesignTab.ToString())
+            {
+                //extract the index of the design region, and set the CurrentDesignTab index
+                tabControl.CurrentDesignTab = int.Parse(e.Region.Name.Substring(HEADER_PREFIX.Length));
+
+                //then after update the design time HTML
+                base.UpdateDesignTimeHtml();
+            }
+        }
+
+
+        public override string GetEditableDesignerRegionContent(EditableDesignerRegion region)
+        {
+            // Get a reference to the designer host
+            IDesignerHost host = (IDesignerHost)(new Component().Site.GetService(typeof(IDesignerHost)));
+
+            //only if known host, and we have some TabPages
+            if (host != null && tabControl.TabPages.Count > 0)
+            {
+                //we need to get the contents of the TabBody of the tabControl
+
+                ITemplate template = tabControl.TabPages[0].TabBody;
+
+
+                if (region.Name.StartsWith(CONTENT_PREFIX))
+                {
+                    //get the template of the selected tab
+
+                    //extract the tab index of the edited region, from the region name.
+                    int tabIndex = int.Parse(region.Name.Substring(CONTENT_PREFIX.Length));
+
+                    //switch the design template to the selected tab index.
+                    template = tabControl.TabPages[tabIndex].TabBody;
+                }
+
+                // Persist the template in the design host
+                if (template != null)
+                    return ControlPersister.PersistTemplate(template, host);
+            }
+
+            return String.Empty;
+        }
+
+        public override void SetEditableDesignerRegionContent(EditableDesignerRegion region, string content)
+        {
+            if (content == null)
+                return;
+
+            // Get a reference to the designer host
+            IDesignerHost host = (IDesignerHost)(new Component().Site.GetService(typeof(IDesignerHost)));
+            if (host != null)
+            {
+                // Create a template from the content string
+                ITemplate template = ControlParser.ParseTemplate(host, content);
+
+                if (template != null)
+                {
+                    // Determine which region should get the template
+                    if (region.Name.StartsWith(CONTENT_PREFIX))
+                    {
+                        int tabIndex = int.Parse(region.Name.Substring(CONTENT_PREFIX.Length));
+                        //set back the template for the selected tab body
+                        tabControl.TabPages[tabIndex].TabBody = template;
+                    }
+                }
+            }
+        }
+        #endregion implementations
+    }
+}
